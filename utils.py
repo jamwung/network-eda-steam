0af69ffb7e90e8685c7c39023a2d9acd139cb915
Jam Wong <JWong.MSDS2023@aim.edu>
2023-07-31 20:28:01 +0000

    added data, utils script, 2 ipynbs, gitignore.

---
+++ utils.py
@@ -0,0 +1,19 @@
+import itertools
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import networkx as nx
+from tqdm.notebook import tqdm
+
+def explode_df(df : pd.DataFrame, col1 : str, col2 : str) -> pd.DataFrame:
+    """
+    Explode the non-atomic entries in col1 and col2 of df.
+    """
+    return (
+        df[[col1, col2]]
+        .dropna()
+        .applymap(lambda x: x.split(';'))
+        .explode(col1, ignore_index=False)
+        .explode(col2, ignore_index=False)
+    )
